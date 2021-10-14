99e47eabb1c1e3dd7868e60fcf1660a34f3a2667
Khuslent Boldbaatar <k.boldbaatar@ntt.com>
2021-10-15 01:13:49 +0900

    定数ファイルを作成

---
+++ src/main/java/constants/AttributeConst.java
@@ -0,0 +1,58 @@
+package constants;
+
+public enum AttributeConst {
+    //フラッシュメッセージ
+    FLUSH("flush"),
+
+    //一覧画面共有
+    MAX_ROW("maxRow"),
+    ERR("errors"),
+
+    //ログイン中の従業員
+    EMPLOYEE("employee"),
+    EMPLOYEES("emplpyees"),
+    EMP_COUNT("employees_count"),
+    EMP_ID("id"),
+    EMP_CODE("code"),
+    EMP_PASS("password"),
+    EMP_NAME("name"),
+    EMP_ADMIN_FLAG("admin_flag"),
+
+    //管理者フラグ
+    ROLE_ADMIN(1),
+    ROLE_GENERAL(0),
+
+    //削除フラグ
+    DEL_FLAG_TRUE(1),
+    DEL_FLAG_FALSE(0),
+
+    //日報管理
+    REPORT("report"),
+    REPORTS("reports"),
+    REP_COUNT("reports_count"),
+    REP_ID("id"),
+    REP_DATE("report_date"),
+    REP_TITLE("title"),
+    REP_CONTENT("content");
+
+    private final String text;
+    private final Integer i;
+
+    private AttributeConst(final String text) {
+        this.text = text;
+        this.i = null;
+    }
+
+    private AttributeConst(final Integer i) {
+        this.text = null;
+        this.i = i;
+    }
+
+    public String getValue() {
+        return this.text;
+    }
+
+    public Integer getIntegerValue() {
+        return this.i;
+    }
+}
